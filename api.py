186b2e1aa54c065f5eb7b47bfed7ee64bad2cdb1
przemyslaw.kozlowski <przemyslaw.kozlowski@xebia.com>
2025-04-23 21:49:54 +0200

    resolving cors issue

--- api.py
+++ api.py
@@ -3,8 +3,10 @@ import os
 from logging import debug
 
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, origins=["https://uwolnic-bonusa-petycja.onrender.com"])
 DATA_FILE = "data.json"
 
 
